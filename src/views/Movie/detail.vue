13cc4894216ae626d966ab604af4388f8b545176
fangyun300 <694476141@qq.com>
2019-07-25 10:15:46 +0800

    bug clear

--- src/views/Movie/detail.vue
+++ src/views/Movie/detail.vue
@@ -4,7 +4,7 @@
 	        <Loading v-if="isLoading"></Loading>
 	        <div v-else id="content" class="contentDetail">
 	        	<div class="detail_list">
-	        		<div class="detail_list_bg"></div>
+	        		<div class="detail_list_bg" :style="{'background-image': 'url('+detailList.img.replace(/w\.h/, '148.208')+')'}"></div>
 	        		<div class="detail_list_filter"></div>
 	        		<div class="detail_list_content">
 	        			<div class="detail_list_img">
